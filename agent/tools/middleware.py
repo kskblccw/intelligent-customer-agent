c7efdc86fd6b7fe8b2f846b96b7e9293019138f4
kskblccw <295192176+kskblccw@users.noreply.github.com>
2026-07-10 18:04:08 +0800

    中间件工具创建

--- agent/tools/middleware.py
+++ agent/tools/middleware.py
@@ -1,1 +1,14 @@
 #中间件开发
+from langchain.agents.middleware import wrap_tool_call
+
+@wrap_tool_call
+def monitor_tool():  #工具执行的监控
+    pass
+
+
+def log_before_model():   #在模型执行前输出日志
+    pass
+
+
+def report_prompt_switch():  #动态切换提示词
+    pass
