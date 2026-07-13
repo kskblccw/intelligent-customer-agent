429bda447306e292664cf6cb4244214bd36fafaf
kskblccw <295192176+kskblccw@users.noreply.github.com>
2026-07-13 17:03:14 +0800

    1.中间件开发 2.向量库完善 3.UI界面-stremlit开发 4.agent联调 5.引入高德api获取实时城市和天气信息

--- agent/tools/middleware.py
+++ agent/tools/middleware.py
@@ -1,14 +1,46 @@
 #中间件开发
-from langchain.agents.middleware import wrap_tool_call
+from typing import Callable
+
+from langchain.agents import AgentState
+from langchain.agents.middleware import wrap_tool_call, before_model, dynamic_prompt, ModelRequest
+from langchain.tools.tool_node import ToolCallRequest
+from langchain_core.messages import ToolMessage
+from langgraph.runtime import Runtime
+from langgraph.types import Command
+
+from utils.logger_handler import logger
+from utils.prompt_loader import load_report_prompts,load_system_prompts
+
 
 @wrap_tool_call
-def monitor_tool():  #工具执行的监控
-    pass
+def monitor_tool(request:ToolCallRequest,handler:Callable[[ToolCallRequest],ToolMessage | Command]) -> ToolMessage | Command :  #工具执行的监控
+    logger.info(f"[monitor_tool]执行工具：{request.tool_call['name']}")
+    logger.info(f"[monitor_tool]传入参数：{request.tool_call['args']}")
+    try:
+         result =  handler(request)
+         logger.info(f"[monitor_tool]执行工具：{request.tool_call['name']}调用成功")
+         if request.tool_call["name"] == "fill_context_for_report":
+             request.runtime.context['report'] = True
+         return result
+
+    except Exception as e:
+        logger.error(f"工具{request.tool_call['name']}调用失败，原因是：{str(e)}")
+        raise e
 
 
-def log_before_model():   #在模型执行前输出日志
-    pass
+@before_model
+def log_before_model(
+        state:AgentState,#整个agent智能体中的状态记录
+        runtime:Runtime, #记录了整个执行过程中的上下文信息
+):   #在模型执行前输出日志
+    logger.info(f"[log_before_model]即将调用模型，带有{len(state['messages'])}条消息")
+    logger.debug(f"[log_before_model]{type(state['messages'][-1]).__name__} | {state['messages'][-1].content.strip()}")
+    return None
 
+@dynamic_prompt   #每一次在生成提示词之前，调用此函数
+def report_prompt_switch(request:ModelRequest):  #动态切换提示词
+    is_report = request.runtime.context.get("report", False)
+    if is_report:   #说明调用了报告总结的工具，我们需要切换提示词
+        return load_report_prompts()
 
-def report_prompt_switch():  #动态切换提示词
-    pass
+    return load_system_prompts()
--- rag/vector_store.py
+++ rag/vector_store.py
@@ -17,7 +17,7 @@ class VectorStoreService:
         self.vector_store = Chroma(
             collection_name=chroma_config['collection_name'],
             embedding_function=embedding_model,
-            persist_directory=chroma_config['persist_directory'],
+            persist_directory=get_abs_path(chroma_config['persist_directory']),
         )
         self.spliter = RecursiveCharacterTextSplitter(
             chunk_size=chroma_config['chunk_size'],
